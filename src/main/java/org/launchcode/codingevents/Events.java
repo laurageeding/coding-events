024a20a74d940a188edbd91c52428b4c6a15c9a2
Laura Geeding <laurageeding@Lauras-MacBook-Pro.local>
2022-05-16 15:31:29 -0500

    init

---
+++ src/main/java/org/launchcode/codingevents/Events.java
@@ -0,0 +1,27 @@
+package org.launchcode.codingevents;
+
+public class Events {
+    private String name;
+    private String description;
+
+    public Events(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,32 +1,45 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.Events;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
-    public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("1");
-        events.add("2");
-        events.add("3");
-        events.add("4");
-        model.addAttribute("events", events);
-        return "events/index";
+    public String displayAllEvents(Model model) {
+//        model.addAttribute("events", events);
+        model.addAttribute("event", Arrays.asList(
+                new Events("LaDee", "Dah"),
+                new Events("BuccaDe", "Beppo"),
+                new Events("Leedell", "Lee")
+        ));
+        return "events/eventsExerciseTable";
     }
 
-    //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String displayCreateEventForm() {
         return "events/create";
     }
+
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
+
 
 }
